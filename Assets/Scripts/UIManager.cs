591aa14dad8c279c0c1fccffcaf5b191b190e383
MorganHenryJames <morganhmjames@gmail.com>
2023-01-13 17:48:12 -0500

    Menu buttons added with multiplayer functions

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -4,27 +4,5 @@ using UnityEngine;
 
 public class UIManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    public GameObject menu; 
-    void Start()
-    {
-        ShowMenu();
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-    public void ShowMenu() 
-    {
-        Instantiate(menu);
-    }
-
-    public void HideMenu()
-    {
-        Destroy(menu);
-    }
+   
 }
