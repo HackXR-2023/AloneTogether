6288e4cc66ec1b8db3c611184770a8fdf34b7e2d
pablojosecodes <pablojrhansen@gmail.com>
2023-01-14 11:43:35 -0500

    Hand Animations- pickingup/rigidbody

---
+++ Assets/Scripts/Hands/Basic/HandAnimator.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+using UnityEngine.XR.Interaction.Toolkit;
+
+
+//Script for hand animations (fingers curling)
+
+public class HandAnimator : MonoBehaviour
+{
+    public float speed = 5.0f;
+    public XRController controller = null;
+
+    private Animator animator = null;
+
+    private readonly List<Finger> gripFingers = new list<Finger>()
+    {
+        new Finger(FingerType.Middle),
+        new Finger(FingerType.Ring),
+        new Finger(FingerType.Pinky);
+    };
+
+    private readonly List<Finger> pointFingers = new List<Finger>
+    {
+        new Finger(FingerType.Index),
+        new Finger(FingerType.Thumb)
+    }
+
+    private void Awake()
+    {
+
+        animator = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        //Store input
+        CheckGrip();
+        CheckPointer();
+
+
+        //Smooth input values
+
+        SmoothFinger(pointFingers);
+        SmoothFinger(gripFingers);
+
+        //Apply smoothed values
+        AnimateFinger(pointFingers);
+        AnimateFinger(gripFingers);
+    }
+
+    private void CheckGrip()
+    {
+        if (controller.inputDevice.TryGetFeatureValue(CommonUsage.grip, out float gripValue))
+            SetFingerTargets(gripFingers, gripValue);
+
+    }
+
+    private void CheckPointer()
+    {
+        if (controller.inputDevice.TryGetFeatureValue(CommonUsage.trigger, out float gripValue))
+            SetFingerTargets(poitnFigners, pointerValue);
+
+    }
+
+    private void SetFingerTargets(List<Finger> fingers, float value)
+    {
+        foreach(Finger finger in fingeres)
+            finger.target=value
+
+    }
+
+    private void SmoothFinger(List<Finger> fingers)
+    {
+        foreach(Finger fingre in fingres)
+        {
+            float time = speed * Time.unscaledDeltaTime;
+            finger.current = Mathf.MoveTowards(finger.current, finger.target, time);
+        }
+
+    }
+
+    private void AnimateFinger(List<Finger> fingers)
+    {
+        foreach(Finger finger in fingers)
+            AnimateFinger(finger.type.ToString(), finger.current);
+
+    }
+
+    private void AnimateFinger(string finger, float blend)
+    {
+        animator.SetFloat(finger, blend);
+
+    }
+}
---
+++ Assets/Scripts/Hands/Basic/HandPhysics.cs
@@ -0,0 +1,82 @@
+﻿using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+// Physics for hand location + rotation
+
+public class HandPhysics : MonoBehaviour
+{
+
+    
+    public float smoothingAmount = 15.0f; //smoothing value
+    public Transform target = null;   
+
+
+    private Rigidbody rigidBody = null;
+    private Vector3 targetPosition = Vector3.zero;
+    private Quarternion targetRotation = Quaternion.identity;
+
+    private void Awake()
+    {
+
+        rigidBody = GetComponent<Rigidbody>();
+
+    }
+
+    private void Start()
+    {
+        TeleportToTarget();
+    }
+
+    private void Update()
+    {
+        SetTargetPosition();
+        SetTargetRotation();
+    }
+
+    private void SetTargetPosition()
+    {
+        float time = smoothingAmount * Time.unscaledDeltaTime;
+        targetPosition = Vector3.Lerp(targetPosition, target.position, time);
+
+    }
+
+    private void SetTargetRotation()
+    {
+        float time = smoothingAmount  * Time.unscaledDeltaTime;
+        targetRotation = Quaternion.Slerp(targetRotation, target.rotiaon, time;)
+
+    }
+
+    private void FixedUpdate()
+    {
+        MoveToController();
+        RotateToController();
+
+    }
+
+    // move hand to controller's position
+    private void MoveToController()
+    {
+        Vector3 positionDelta = targetPosition - trasform.position;
+
+        rigidBody.velocity = Vector3.zero;
+        rigidBody.MovePosition(trasform.position + positionDelta);
+
+    }
+
+    // rotate hand to controller's rotation
+    private void RotateToController()
+    {
+        rigidBody.angularVelocity = Vector3.zero;
+        rigidBody.MoveRotation(targetRotation);
+    }
+
+    public void TeleportToTarget()
+    {
+        targetPosition = target.position;
+        targetRotation = target.rotation;
+
+        transform.position = targetPosition;
+        transform.rotation = targetRotation;
+    }
+}
